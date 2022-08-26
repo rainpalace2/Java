7539e060d41cd16c15a6a82c8434e32c316f335d
EC2 Default User <110389642+rainpalace2@users.noreply.github.com>
2022-08-26 06:56:03 +0000

    [Add]books/index,show

--- Rails6-bookers2-debug-main/app/controllers/books_controller.rb
+++ Rails6-bookers2-debug-main/app/controllers/books_controller.rb
@@ -2,15 +2,20 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @user = current_user
+    @books = Book.new
   end
 
   def index
     @books = Book.all
+    @user = current_user
+    @book = Book.new
   end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
+    @user = current_user
     if @book.save
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
@@ -41,6 +46,6 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 end
--- Rails6-bookers2-debug-main/app/controllers/users_controller.rb
+++ Rails6-bookers2-debug-main/app/controllers/users_controller.rb
@@ -13,20 +13,29 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+    if @user == current_user
+      render "edit"
+      flash[:notice] = "You have updated user successfully."
+    else
+      redirect_to user_path(current_user)
+    end
   end
 
   def update
+    @user = User.find(params[:id])
+    @user = current_user
     if @user.update(user_params)
-      redirect_to users_path(@user), notice: "You have updated user successfully."
+      redirect_to user_path(@user), notice: "You have updated user successfully."
     else
-      render "show"
+      render :edit
     end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :introduction, :profile_image)
+    params.require(:user).permit(:name, :introduction, :profile_image, :title)
   end
 
   def ensure_correct_user
