342fe05acd919198acfc5a78dfd7b3b537b83e84
EC2 Default User <110389642+rainpalace2@users.noreply.github.com>
2022-08-28 12:57:02 +0000

    [Add]投稿詳細画面のいいね

--- Rails6-bookers2-debug-main/app/controllers/favorites_controller.rb
+++ Rails6-bookers2-debug-main/app/controllers/favorites_controller.rb
@@ -9,7 +9,7 @@ class FavoritesController < ApplicationController
 
   def destroy
     book = Book.find(params[:book_id])
-    fovorite = current_user.favorites.find_by(book_id: book.id)
+    favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
     redirect_to book_path(book)
   end
