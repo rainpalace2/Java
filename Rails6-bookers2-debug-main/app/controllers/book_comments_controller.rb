02fb3380cf443ff368a2663981ff3396166dce98
EC2 Default User <110389642+rainpalace2@users.noreply.github.com>
2022-09-08 05:07:56 +0000

    Javaについて

--- Rails6-bookers2-debug-main/app/controllers/book_comments_controller.rb
+++ Rails6-bookers2-debug-main/app/controllers/book_comments_controller.rb
@@ -7,13 +7,13 @@ class BookCommentsController < ApplicationController
    comment.save
    render :book_comments
   end
-
+  
   def destroy
   BookComment.find(params[:id]).destroy
   @book = Book.find(params[:book_id])
   render :book_comments
   end
-
+  
   private
 
   def book_comment_params
